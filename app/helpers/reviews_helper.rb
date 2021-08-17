c633ed95b5bed7304b11f4751bf7c303b475534d
Dexter <dex429.dc@gmail.com>
2021-08-17 19:58:40 -0400

    add reviews helper

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,10 @@
 module ReviewsHelper
+    
+    def display_header(review)
+        if params[:strain_id]
+            content_tag(:h1, "Add a Review for #{review.strain.name} -  #{review.strain.category}")
+        else
+          content_tag(:h1, "Create a review")
+        end
+    end
 end
