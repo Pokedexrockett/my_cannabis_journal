93575008157cdd2cf9c2d8c68c0406e9e989d362
Dexter <dex429.dc@gmail.com>
2021-08-17 19:33:01 -0400

    add oder scope, grower attributes, grower_name, name_and_grower

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -5,4 +5,26 @@ class Strain < ApplicationRecord
     has_many :users, through: :reviews
 
     validates :name, :category, :thc, :cbd, presence: true
+
+    scope :order_by_rating, -> {left_joins(:reviews).group(:id).order('avg(pot_leaves) desc')}
+
+    def self.alpha
+        order(:name) 
+    end
+
+    def grower_attributes=(attributes)
+        self.grower = Grower.find_or_create_by(attributes) if !attributes['name'].empty?
+        self.grower 
+    end
+
+ 
+
+    def grower_name
+        grower.try(:name)
+    end
+    
+    def name_and_grower
+        "#{name} - #{grower.try(:name)}"
+    end
+
 end
